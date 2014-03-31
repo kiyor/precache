7e5cbb331b94cf7154b5c2fa17bef007d64ef223
kiyor <cyfkiyor@gmail.com>
2014-03-31 17:22:33 +0000

    add new feature

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 - Creation Date : 03-25-2014
 
-- Last Modified : Thu 27 Mar 2014 11:27:32 PM UTC
+- Last Modified : Mon 31 Mar 2014 05:22:22 PM UTC
 
 - Created By : Kiyor
 
@@ -27,23 +27,32 @@ dir = /data/user/public_html
 rootdir = /data/user/public_html
 type = f
 mtime = 1
+ctime = 1
 
 [precache]
 hosts = host1.com host2.com host3.com
 vhost = vhost.com
 proc = 8
-
+; this is the header you setup in nginx like `add_header X-Cache $upstream_cache_status;`
+cachestatus = X-Cache
+; header request timeout
+timeout = 3s
 
 ```
 
 then run
 
 `precache -f config.ini`
+  
 this command will give you file list, you able to check file list before you do anything
+  
 
 `precache -f config.ini -do`
+  
 this command will starting requesting file
 
+you can add flag -purgefirst to purge file before precache, it use for some time.
+  
 currently it will compare last mod time and content size, if the file not getting X-Cache header HIT, it will request again after 1 seconds
 
 currently not do purge, but code already write, change whatever you need.
@@ -51,10 +60,9 @@ currently not do purge, but code already write, change whatever you need.
 You'd better add proxy_cache_lock in your nginx config, otherwise it will fucked up your nginx server;
   
 ```
-
 proxy_cache_lock on;
 proxy_cache_lock_timeout 300s;
-
+proxy_cache_use_stale updating;
 
 ```
 
--- precache.go
+++ precache.go
@@ -6,7 +6,7 @@
 
 * Creation Date : 03-19-2014
 
-* Last Modified : Thu 27 Mar 2014 11:20:23 PM UTC
+* Last Modified : Sat 29 Mar 2014 11:10:35 PM UTC
 
 * Created By : Kiyor
 
@@ -15,12 +15,15 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 package main
 
 import (
+	"crypto/md5"
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"github.com/dustin/go-humanize"
 	"github.com/kiyor/gfind/lib"
 	"github.com/vaughan0/go-ini"
 	"github.com/wsxiaoys/terminal/color"
+	"io"
 	"net"
 	"net/http"
 	"net/url"
@@ -86,17 +89,22 @@ var (
 	fvhost *string = flag.String("vhost", "client.com", "vhost hostname")
 	do     *bool   = flag.Bool("do", false, "do request")
 
-	fproc *int    = flag.Int("proc", 2, "worker")
+	fproc *int    = flag.Int("proc", 0, "worker")
 	conff *string = flag.String("f", "conf.ini", "conf file location")
+
+	purge           *bool   = flag.Bool("purgefirst", false, "do purge request before anything")
+	ngxSecurityLink *string = flag.String("ngxsecuritylink", "", "Nginx Security Link security key")
+	cStatus         *string = flag.String("cachestatus", "X-Cache", "Nginx Upstream status Header")
+	timeOut         *string = flag.String("timeout", "0s", "request timeout, default will overwrite to 3s")
 
 	verbose *bool = flag.Bool("v", false, "verbose")
 
-	rootdir, vhost   string
-	hosts, httphosts []string
-	proc             int
-	clients          []client
-	timeout          = 3 * time.Second
-	transport        = http.Transport{
+	rootdir, vhost, security, cacheStatus string
+	hosts, httphosts                      []string
+	proc                                  int
+	timeout                               time.Duration
+	clients                               []client
+	transport                             = http.Transport{
 		Dial: dialTimeout,
 	}
 )
@@ -107,6 +115,16 @@ func readConff() {
 	hs, _ := f.Get("precache", "hosts")
 	hosts = strings.Split(hs, " ")
 	vhost, _ = f.Get("precache", "vhost")
+	security, _ = f.Get("precache", "security")
+	cacheStatus, _ = f.Get("precache", "cachestatus")
+	tiout, ok := f.Get("precache", "timeout")
+	if !ok {
+		timeout = 3 * time.Second
+	} else {
+		timeout, err = time.ParseDuration(tiout)
+		chkErr(err)
+	}
+
 	p, ok := f.Get("precache", "proc")
 	if !ok {
 		p = "2"
@@ -124,8 +142,22 @@ func readConff() {
 	if *fvhost != "client.com" {
 		vhost = *fvhost
 	}
-	if ok && *fproc == 2 {
+	if *ngxSecurityLink != "" {
+		security = *ngxSecurityLink
+	}
+	if *timeOut != "0s" {
+		timeout, err = time.ParseDuration(*timeOut)
+		chkErr(err)
+	}
+	if *cStatus != "X-Cache" {
+		cacheStatus = *cStatus
+	} else if cacheStatus == "" {
+		cacheStatus = "X-Cache"
+	}
+	if ok && *fproc == 0 {
 
+	} else if *fproc == 0 {
+		proc = 2
 	} else {
 		proc = *fproc
 	}
@@ -136,9 +168,6 @@ func init() {
 	flag.Parse()
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	hs := *fhosts
-	hosts = strings.Split(hs, " ")
-	vhost = *fvhost
 	readConff()
 	for i := 0; i < proc; i++ {
 		clients = append(clients, initClient())
@@ -179,10 +208,7 @@ func Gourl(host string, fs []gfind.MyFile, wg *sync.WaitGroup) {
 		w.add(1)
 		id := getClient(clients)
 		go func(id int, v gfind.MyFile, w *worker, k int) {
-			// 			if *verbose {
-			// 				fmt.Println("id:", id, v.Path)
-			// 			}
-			clients[id].gogogo(host, v, k)
+			clients[id].gogogo(host, v, k, id)
 			w.done()
 		}(id, v, &w, k)
 	}
@@ -190,27 +216,61 @@ func Gourl(host string, fs []gfind.MyFile, wg *sync.WaitGroup) {
 	wg.Done()
 }
 
-func (c *client) purge(host string, f gfind.MyFile) {
+func (c *client) purge(host string, f gfind.MyFile, k int, id int) {
 	var Url *url.URL
 	Url, err := url.Parse(host)
 	chkErr(err)
 	Url.Path += "/purge"
 	Url.Path += f.Relpath
-	req, err := http.NewRequest("HEAD", Url.String(), nil)
+	var u string
+	u = Url.String()
+	if security != "" {
+		u += "?" + ParseNgxSecurityLink(security, host, f)
+	}
+	req, err := http.NewRequest("HEAD", u, nil)
 	req.Close = true
 	if vhost != "client.com" {
 		req.Host = vhost
 	}
-	c.c.Do(req)
+	resp, err := c.c.Do(req)
+	chkErr(err)
+	if resp.StatusCode == 200 {
+		color.Printf("%6v-%-2d @{g}%-17v@{|} %v %v\n", k, id, "PURGE:SUCCESS", Url.String(), f.Size)
+	} else if resp.StatusCode == 404 {
+		color.Printf("%6v-%-2d @{y}%-17v@{|} %v %v\n", k, id, "PURGE:NOFILE", Url.String(), f.Size)
+	}
+
 }
 
-func (c *client) gogogo(host string, f gfind.MyFile, k int) {
+func ParseNgxSecurityLink(key string, host string, f gfind.MyFile) string {
 	var Url *url.URL
 	Url, err := url.Parse(host)
 	chkErr(err)
 	Url.Path += f.Relpath
-	fmt.Println(k, Url.String(), f.Size)
-	req, err := http.NewRequest("HEAD", Url.String(), nil)
+	loc := Url.String()[len(host):]
+	token := key + loc + "9999999999"
+	h := md5.New()
+	io.WriteString(h, token)
+	str := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	r := strings.NewReplacer("/", "_", "=", "", "+", "-")
+	return "st=" + r.Replace(str) + "&e=9999999999"
+}
+
+func (c *client) gogogo(host string, f gfind.MyFile, k int, id int) {
+	if *purge {
+		c.purge(host, f, k, id)
+	}
+	var Url *url.URL
+	Url, err := url.Parse(host)
+	chkErr(err)
+	Url.Path += f.Relpath
+	var u string
+	u = Url.String()
+	if security != "" {
+		u += "?" + ParseNgxSecurityLink(security, host, f)
+	}
+	color.Printf("%6v-%-2d @{y}%-17v@{|} %v %v\n", k, id, "START", Url.String(), f.Size)
+	req, err := http.NewRequest("HEAD", u, nil)
 	req.Close = true
 	if vhost != "client.com" {
 		req.Host = vhost
@@ -218,59 +278,78 @@ func (c *client) gogogo(host string, f gfind.MyFile, k int) {
 	for true {
 		resp, err := c.c.Do(req)
 		if err != nil {
-			fmt.Println(err.Error())
+			color.Printf("@{g}%v\n", err.Error())
+			time.Sleep(5 * time.Second)
 			continue
 		}
 
 		defer resp.Body.Close()
 
 		if resp.StatusCode != 200 {
-			color.Printf("@{g}%v %v %v %v\n", k, Url.String(), "StatusCode", resp.StatusCode)
+			color.Printf("@{g}%6v-%-2d %v %v %v\n", k, id, Url.String(), "StatusCode", resp.StatusCode)
+			time.Sleep(5 * time.Second)
 			continue
 		}
 		var b1, b2, b3 bool
 
-		if val, ok := resp.Header["X-Cache"]; ok {
+		if val, ok := resp.Header[cacheStatus]; ok {
 			if val[0] == "HIT" {
 				b1 = true
+			} else if val[0] == "UPDATING" || val[0] == "MISS" {
+				t, err := time.ParseDuration(fmt.Sprintf("%dms", f.Size/100000))
+				chkErr(err)
+				if *verbose {
+					color.Printf("%6v-%-2d @{r}%7s:%-9s@{|} %v Size:%v SLEEP:%vms\n", k, id, cacheStatus, val[0], Url.String(), humanize.IBytes(uint64(f.Size)), f.Size/100000)
+				}
+				time.Sleep(t)
+				continue
 			} else {
 				if *verbose {
-					color.Printf("@{r}%v %v %v %v %v\n", k, Url.String(), "X-Cache", val[0], humanize.IBytes(uint64(f.Size)))
+					color.Printf("%6v-%-2d @{r}%7s:%-9s@{|} %v Size:%v SLEEP\n", k, id, cacheStatus, val[0], Url.String(), humanize.IBytes(uint64(f.Size)))
 				}
+				time.Sleep(2 * time.Second)
 				continue
 			}
 		} else {
 			if *verbose {
-				color.Printf("@{r}%v %v %v\n", k, Url.String(), "X-Cache NULL")
+				color.Printf("%6v-%-2d @{r}%7s:%-9v@{|} %v SLEEP\n", k, id, cacheStatus, "NULL", Url.String())
 			}
+			time.Sleep(5 * time.Second)
 			continue
 		}
 		if val, ok := resp.Header["Last-Modified"]; ok {
 			if val[0] == time2fmt(timespec2time(f.Stat.Mtim)) {
 				b2 = true
 			} else {
-				color.Printf("@{b}%v %v %v %v %v %v\n", k, Url.String(), " Header: ", val[0], " File: ", time2fmt(timespec2time(f.Stat.Mtim)))
-				c.purge(host, f)
+				color.Printf("@{b}%6v-%-2d %v %v %v %v %v\n", k, id, Url.String(), " Header: ", val[0], " File: ", time2fmt(timespec2time(f.Stat.Mtim)))
+				c.purge(host, f, k, id)
 				continue
-				// 				b2 = true
 			}
+		} else {
+			color.Printf("@{g}%6v-%-2d %-17v %v %v\n", k, id, "NO:Last-Modified", Url.String())
 		}
 		if val, ok := resp.Header["Content-Length"]; ok {
 			s, _ := strconv.Atoi(val[0])
 			if int64(s) == f.Size {
 				b3 = true
 			} else {
-				color.Printf("@{b}%v %v %v %v %v %v\n", k, Url.String(), " Header: ", val[0], " File: ", f.Size)
-				c.purge(host, f)
+				color.Printf("@{b}%6v-%-2d %v %v %v %v %v\n", k, id, Url.String(), " Header: ", val[0], " File: ", f.Size)
+				c.purge(host, f, k, id)
 				continue
-				// 				b3 = true
 			}
+		} else {
+			color.Printf("@{g}%6v-%-2d %-17v %v %v\n", k, id, "NO:Content-Length", Url.String())
 		}
+
+		// if cache hit, size and last mod match, then break loop
 		if b1 && b2 && b3 {
+			if *verbose {
+				color.Printf("%6v-%-2d @{g}%-17v@{|} %v %v\n", k, id, "FINISH", Url.String(), f.Size)
+			}
 			break
 		}
 
-		time.Sleep(1000 * time.Millisecond)
+		time.Sleep(5000 * time.Millisecond)
 	}
 	c.unlock()
 }
